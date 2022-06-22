e50d763642f911c6d7592303aaa3f2ed876de73e
web3allday <99647430+web3allday@users.noreply.github.com>
2022-06-22 11:54:58 +0100

    rpc change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   const network = WalletAdapterNetwork.Devnet;
 
   // You can also provide a custom RPC endpoint.
-  const endpoint = clusterApiUrl(network);
+  const endpoint = "https://devnet.genesysgo.net/";
 
   // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
   // Only the wallets you configure here will be compiled into your application, and only the dependencies
